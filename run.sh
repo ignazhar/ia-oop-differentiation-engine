9bdf64e0f6194f993f55a8c9bb35906fb1f81f3f
Ihnat Zharikhin <ignazhar@gmail.com>
2025-12-30 19:06:36 +0100

    initial setup commit

---
+++ run.sh
@@ -0,0 +1,2 @@
+mvn compile -f "d:\Ignat\cam\ia\oop\differentiation_engine\pom.xml" &&
+\mvn test -f "d:\Ignat\cam\ia\oop\differentiation_engine\pom.xml"
---
+++ src/main/java/differentiation_engine/Expression.java
@@ -0,0 +1,9 @@
+package differentiation_engine;
+
+/*
+I chose abstract class, not interface for Expression, 
+because Expression itself has defined behaviour(can be evaluated, differentiated, printed).
+*/
+public abstract class Expression {
+    
+}
