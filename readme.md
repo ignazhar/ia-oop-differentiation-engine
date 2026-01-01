1c0030777cb8329d16e2bdeb932a17da76fa2c64
Ihnat Zharikhin <ignazhar@gmail.com>
2026-01-01 17:12:30 +0100

    mvp version finished

--- readme.md
+++ readme.md
@@ -1,2 +1,22 @@
 # ia-oop-differentiation-engine
 Differentiation engine repo for IA OOP class
+
+## Features:
+### Expressions
+* Constant value
+* Variable
+* Addition of 2 expressions
+* Multiplication of 2 expressions
+* Sin, Cos, Tan functions
+* General exponentiation f(x)^g(x)
+* Natural Logarithm
+### Other
+* Easy simplifaction steps for multiplication and addition expressions
+* Easy bracket optimization using atomicity of expressions
+
+## TODO:
+* Add specific exponentiation for c^f(x) and f(x)^c
+* Generalize addition and multiplication to arrays of expression
+* Build on simplification based on previous point
+* Add command-line communication possibility(life-cycle parsing)
+* Add proper testing list of examples
--- src/test/java/differentiation_engine/AppTest.java
+++ src/test/java/differentiation_engine/AppTest.java
@@ -15,6 +15,43 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue(true);
+        Expression c1 = new Const(1);
+        Expression c2 = new Const(2);
+        Expression c3 = new Const(3);
+        Expression c4 = new Const(4);
+        Expression c5 = new Const(5);
+        Expression cm1 = new Const(-1);
+        Expression c10 = new Const(10);
+        // assertTrue(true);
+        // Variable x = new Variable("x");
+        // Variable y = new Variable("y");
+        // Expression expr1 = new Add(x, y);
+        // Expression diff1 = expr1.differentiate(x);
+
+        // System.out.println(expr1.toString());
+        // System.out.println(diff1.toString());
+
+        Variable x = new Variable("x");
+        Expression expr2 = new Multiply(x, new Sin(new Multiply(new Const(10), x)));
+        Expression diff2 = expr2.differentiate(x);
+        Expression diff22 = diff2.differentiate(x);
+
+        System.err.println(expr2.toString());
+        System.err.println(diff2.toString());
+        System.err.println(diff22.toString());
+        
+        diff2 = diff2.Simplify();
+        diff22 = diff22.Simplify();
+        
+        System.err.println("-------------");
+        System.err.println(diff2.toString());
+        System.err.println(diff22.toString());
+
+        System.err.println("=============");
+        
+        Expression p = new Exponent(x, new Logarithm(new Multiply(c2, x)));
+        Expression dp = p.differentiate(x).Simplify();
+        System.err.println(p);
+        System.err.println(dp);
     }
 }
