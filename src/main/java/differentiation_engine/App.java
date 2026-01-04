8e509cf47a7ce2f53bc71d62a164fd545a62b3b3
Ihnat Zharikhin <ignazhar@gmail.com>
2026-01-04 15:47:25 +0100

    simple main running logic implemented

--- src/main/java/differentiation_engine/App.java
+++ src/main/java/differentiation_engine/App.java
@@ -1,5 +1,11 @@
 package differentiation_engine;
 
+import java.util.Scanner;
+
+import differentiation_engine.expressions.Expression;
+import differentiation_engine.expressions.Variable;
+import differentiation_engine.parser.Parser;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,19 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        Parser parser = new Parser();
+        
+        System.out.println("Enter expression for differentiation:");
+        String input = scanner.next();
+        Expression expr = parser.parse(input).Simplify();
+        System.out.println("Parsed expression: " + expr);
+
+        System.out.println("Enter variable for differentiation:");
+        Variable var = new Variable(scanner.next());
+        
+        System.out.println(expr.differentiate(var).Simplify());
+
+        scanner.close();
     }
 }
