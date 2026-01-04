108a2e7fdf4ebd73b3fb63e5a88828f8b6c50898
Ihnat Zharikhin <ignazhar@gmail.com>
2026-01-04 21:59:22 +0100

    Revert "fixed parser package destination"
    This reverts commit 768ba60fd3902d78365668be92043b1668810fc4.
    
    commit to revert back to fix the mistake of not moving destination path
    of Parser package correctly

--- src/main/java/differentiation_engine/parser/MutableInteger.java
+++
@@ -1,9 +0,0 @@
-package differentiation_engine.parser;
-
-public class MutableInteger {
-    private int value;
-    public MutableInteger(int value) {this.value = value;}
-    public int getValue() {return this.value;}
-    public MutableInteger inc() {this.value ++; return this; }
-    public MutableInteger inc(int add) {this.value += add; return this; }
-}
--- src/main/java/differentiation_engine/parser/SignItem.java
+++
@@ -1,7 +0,0 @@
-package differentiation_engine.parser;
-
-final class SignItem implements Item {
-    private char sign;
-    public SignItem(char sign) {this.sign = sign;}
-    public char getSign() {return this.sign;}
-}
